7c522a739b519e4afc24b9994f4bd6eab3c586ad
Sharllyson <sharllyson.rodrigues@hotmail.com>
2025-10-28 11:48:28 -0300

    Update Server.kt

--- src/main/kotlin/com/codegestao/inventory/Server.kt
+++ src/main/kotlin/com/codegestao/inventory/Server.kt
@@ -12,7 +12,6 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.time.Instant
 import java.util.UUID
 
 fun main() {
